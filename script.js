c61cd978a8c8a05354ab3a2a1fcc95968fc25ea8
rhx1138 <ryanjhendrix@gmail.com>
2022-06-14 17:56:31 -0400

    prompting user and looping num of chars, generating password when user correctly adds at least one parameter

--- script.js
+++ script.js
@@ -1,33 +1,71 @@
-// Assignment code here
 
-
-// we still need multiple choices to select (spec chars etc)
-// how many chars long
-// lowercase, uppercase, numeric, and/or special characters
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
-// var pwdChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-// var pwdLen = 10;
-// var generatePassword = Array(pwdLen).fill(pwdChars).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
+let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
+let alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// will go into the value of charset after user prompt
+let confirmLength = "";
 
 
+// Prompt to confirm how many characters the user would like in their password
 function generatePassword() {
-  var length = 9,
-      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-      retVal = "";
-  for (var i = 0, n = charset.length; i < length; ++i) {
-      retVal += charset.charAt(Math.floor(Math.random() * n));
+  let confirmLength = (prompt("How many characters would you like your password to contain?"));
+
+  // will continue to loop if not within parameters 
+  while (confirmLength <= 7 || confirmLength >= 60) {
+    alert("Password must be between 8-60 characters");
+    let confirmLength = (prompt("How many characters would you like your password to contain?"));
   }
-  return retVal;
+
+  // get parameters from user  
+  let confirmSpecialChar = confirm("Click OK if you would like to include special characters");
+  let confirmNumericChar = confirm("Click OK if you would like to include numeric characters");
+  let confirmAlphaLower = confirm("Click OK if you would like to include lowercase characters");
+  let confirmAlphaUpper = confirm("Click OK if you would like to include uppercase characters");
+  // may need to loop if answer is outside the parameters or possible screen refresh
+  // still printing 'undefined' to the output; possible message there
+  
+
+  let passwordCharacters = []
+
+  if (confirmSpecialChar) {
+    passwordCharacters = passwordCharacters.concat(specialChar)
+  }
+
+  if (confirmNumericChar) {
+    passwordCharacters = passwordCharacters.concat(number)
+  }
+
+  if (confirmAlphaLower) {
+    passwordCharacters = passwordCharacters.concat(alphaLower)
+  }
+
+  if (confirmAlphaUpper) {
+    passwordCharacters = passwordCharacters.concat(alphaUpper)
+  }
+
+  console.log(passwordCharacters)
+
+  // Empty string filled based on for loop selecting random chars from the array
+  let randomPassword = "";
+
+  for (let i = 0; i < confirmLength; i++) {
+    randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    console.log(randomPassword)
+  }
+  return randomPassword;
 }
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
